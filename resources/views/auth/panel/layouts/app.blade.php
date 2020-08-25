79677f7178cb173f40d6f03a122786734fc2a076
thomasvandamme97 <thomas.vandamme97@outlook.com>
2020-08-25 22:26:04 +0200

    admin panel css fix

--- resources/views/auth/panel/layouts/app.blade.php
+++ resources/views/auth/panel/layouts/app.blade.php
@@ -20,34 +20,72 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="d-flex vw-100 vh-100">
-    <nav class="vh-100 bg-dark p-2">
-        <div class="text-center nav-item">
-            <a class="nav-link" href="{{ route('home') }}">portfolio</a>
-        </div>
+    <nav class="vh-100 bg-dark p-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('home') }}">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-door-open-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2v13h1V2.5a.5.5 0 0 0-.5-.5H11zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+                    </svg>
+                    <span class="px-2">Return</span>
+                </a>
+            </li>
+        </ul>
         <hr class="bg-light">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('profile.index') }}">Profile</a>
+                <a class="nav-link d-flex align-items-center" href="{{ route('profile.index') }}">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg>
+                    <span class="px-2">Profile</span>
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('education.index') }}">Education</a>
+                <a class="nav-link d-flex align-items-center" href="{{ route('education.index') }}">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-book-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.261 13.666c.345.14.739-.105.739-.477V2.5a.472.472 0 0 0-.277-.437c-1.126-.503-5.42-2.19-7.723.129C5.696-.125 1.403 1.56.277 2.063A.472.472 0 0 0 0 2.502V13.19c0 .372.394.618.739.477C2.738 12.852 6.125 12.113 8 14c1.875-1.887 5.262-1.148 7.261-.334z"/>
+                    </svg>
+                    <span class="px-2">Education</span>
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('employment.index') }}">Employment</a>
+                <a class="nav-link d-flex align-items-center" href="{{ route('employment.index') }}">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-briefcase-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z"/>
+                        <path fill-rule="evenodd" d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5v1.384l-7.614 2.03a1.5 1.5 0 0 1-.772 0L0 5.884V4.5zm5-2A1.5 1.5 0 0 1 6.5 1h3A1.5 1.5 0 0 1 11 2.5V3h-1v-.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V3H5v-.5z"/>
+                    </svg>
+                    <span class="px-2">Employment</span>
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('skills.index') }}">Skills</a>
+                <a class="nav-link d-flex align-items-center" href="{{ route('skills.index') }}">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-code-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M2 3a2 2 0 0 1 2-2h5.293a1 1 0 0 1 .707.293L13.707 5a1 1 0 0 1 .293.707V13a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3zm7 2V2l4 4h-3a1 1 0 0 1-1-1zM6.646 7.646a.5.5 0 1 1 .708.708L5.707 10l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zm4.708 2l-2-2a.5.5 0 1 0-.708.708L10.293 10l-1.647 1.646a.5.5 0 0 0 .708.708l2-2a.5.5 0 0 0 0-.708z"/>
+                    </svg>
+                    <span class="px-2">Skills</span>
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('softskills.index') }}">Softskills</a>
+                <a class="nav-link d-flex align-items-center" href="{{ route('softskills.index') }}">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-diff-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M2 3a2 2 0 0 1 2-2h5.293a1 1 0 0 1 .707.293L13.707 5a1 1 0 0 1 .293.707V13a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3zm7 2V2l4 4h-3a1 1 0 0 1-1-1zM8 6a.5.5 0 0 1 .5.5V8H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V9H6a.5.5 0 0 1 0-1h1.5V6.5A.5.5 0 0 1 8 6zm-2.5 6.5A.5.5 0 0 1 6 12h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                    <span class="px-2">Softskills</span>
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('projects.index') }}">Projects</a>
+                <a class="nav-link d-flex align-items-center" href="{{ route('projects.index') }}">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-binary-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M2 3a2 2 0 0 1 2-2h5.293a1 1 0 0 1 .707.293L13.707 5a1 1 0 0 1 .293.707V13a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3zm7 2V2l4 4h-3a1 1 0 0 1-1-1zm-1.95 5.885c0 1.415-.548 2.206-1.524 2.206C4.548 13.09 4 12.3 4 10.885c0-1.412.548-2.203 1.526-2.203.976 0 1.524.79 1.524 2.203zM5.526 9.273c-.542 0-.832.563-.832 1.612 0 .088.003.173.006.252l1.56-1.143c-.126-.474-.375-.72-.733-.72zm-.732 2.508c.126.472.372.718.732.718.54 0 .83-.563.83-1.614 0-.085-.003-.17-.006-.25l-1.556 1.146zm6.061.624V13h-3v-.595h1.181V9.5h-.05l-1.136.747v-.688l1.19-.786h.69v3.633h1.125z"/>
+                    </svg>
+                    <span class="px-2">Projects</span>
+                </a>
             </li>
         </ul>
     </nav>
 
-    <main class="container-fluid p-4">
+    <main class="container-fluid p-4 overflow-content">
         @yield('content')
     </main>
 </body>
