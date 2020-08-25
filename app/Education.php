68a47117fbe92a0d5b052ddf4d6c6ca4a40e8b25
thomasvandamme97 <thomas.vandamme97@outlook.com>
2020-08-25 21:57:51 +0200

    education fix

--- app/Education.php
+++ app/Education.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Education extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'school', 'diploma', 'start_year', 'end_year'
+    ];
 }
